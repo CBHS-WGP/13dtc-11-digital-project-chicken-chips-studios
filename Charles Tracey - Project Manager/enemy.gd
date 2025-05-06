924367f374e0e414da814c72e9876fbf10545112
Cartogen <EverittCD21@student.cbhs.school.nz>
2025-05-06 22:22:33 +1200

    Enemy working in my test map
    This means ive set up how to get enemies working EASILY within any tcsn!

--- Charles Tracey - Project Manager/enemy.gd
+++ Charles Tracey - Project Manager/enemy.gd
@@ -2,21 +2,21 @@ extends CharacterBody3D
 
 
 @onready var nav = $NavigationAgent3D
-var speed = 1
+var speed = 2
 var gravity = 9.8
 
 # Code which makes the enemy move towards the player
 func _process(delta):
 	if not is_on_floor():
-		velocity.y -=gravity*delta
+		velocity.y -= gravity * delta
 	else:
 		velocity.y -= 2
 	var next_location = nav.get_next_path_position()
 	var current_location = global_transform.origin
 	var new_velocity = (next_location - current_location).normalized() * speed
-	
-	velocity = velocity.move_toward(new_velocity,0.25)
+	velocity = velocity.move_toward(new_velocity, 0.25)
 	move_and_slide()
 	
-func target_position(target):
-	nav.target_position = target
+func update_target_location(target_location):
+	nav.set_target_position(target_location)
+	#look_at(target_location)
---
+++ Charlie/Maps/Enemy_NAV_Script.gd
@@ -0,0 +1,7 @@
+extends Node3D
+
+@onready var player = $Wayne
+
+
+func _physics_process(_delta):
+	get_tree().call_group("enemy", "update_target_location", player.global_transform.origin)
