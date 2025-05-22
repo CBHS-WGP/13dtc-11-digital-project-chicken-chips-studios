e199dc521a3f2096e394a38c24359ce51f973100
Cartogen <118136480+Cartogen@users.noreply.github.com>
2025-05-22 14:29:09 +1200

    Added code to progress through objectives
    This doesnt actually work yet
    Next steps:
    Player raycast to pick up items.
    Have the item stored in progress manager.
    Allow the item to be used to progress!

--- progression_tracker/progress.gd
+++ progression_tracker/progress.gd
@@ -3,10 +3,20 @@ var current_objective = 1
 # Objectinve 1 varibles
 var objective_1 = 0
 
+var sattelite_activated = false
+
 #Objective 2 varibles
 
 
 func _process(delta):
+	#Progressing past objective 1
 	if Progress.objective_1 >= 5:
+		current_objective = current_objective + 1
+		objective_1 = 0
+	#Progressing past objective 2
+	if Progress.sattelite_activated == true:
+		pass
+	#If objective 3, win the game
+	if Progress.current_objective == 3:
 		get_tree().change_scene_to_file("res://Charlie/UI/credits.tscn")
 		
