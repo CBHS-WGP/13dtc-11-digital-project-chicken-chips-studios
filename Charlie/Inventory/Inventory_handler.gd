0eb137f0dba48ff9b6e320aea53535a0d084d2e2
Cartogen <EverittCD21@student.cbhs.school.nz>
2025-06-03 14:55:25 +1200

    Revert "Followed part 1 and 2"
    This reverts commit e2d0e428f1153d1f00e3badfef0b62b360b97997.

--- Charlie/Inventory/Inventory_handler.gd
+++ Charlie/Inventory/Inventory_handler.gd
@@ -3,7 +3,6 @@ extends Control
 class_name InventoryHandler 
 
 @export var ItemSlotsCount : int = 4
-
 @export var InventoryGrid : GridContainer
 @export var InventorySlotPrefab : PackedScene = preload("res://Charlie/Inventory/Inv_Slot.tscn")
 
