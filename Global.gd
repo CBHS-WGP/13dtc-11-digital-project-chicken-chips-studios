e6c673651468bf8f9b52dfacfe54efe8e93efe4b
Cartogen <EverittCD21@student.cbhs.school.nz>
2025-06-03 14:58:37 +1200

    Revert "Followed part 1 and 2"
    This reverts commit 046a83921b2c6ac98e2956449f7fe7e518c60700.

--- Global.gd
+++ Global.gd
@@ -5,14 +5,12 @@ signal shot
 var inv_open = false
 var sensitivity = 0.15
 var resolution = Vector2(1280, 720)
+var current_areas = 0
 var crouching = false
 var health = 100
 var settings_open = false
-<<<<<<< Updated upstream
 var current_raycast
 var ray_global
-=======
->>>>>>> Stashed changes
 
 func dead(_delta):
 	if health == 0:
