7be1d5b14a2b1864ed1f2e52135ad38bc4c7af27
Cartogen <118136480+Cartogen@users.noreply.github.com>
2025-06-04 12:48:56 +1200

    Code from tutorial 3 added
    Doesnt seem to work though???

--- Charlie/Inventory/Inventory_handler.gd
+++ Charlie/Inventory/Inventory_handler.gd
@@ -2,6 +2,9 @@ extends Control
 
 class_name InventoryHandler 
 
+@export var PlayerBody : CharacterBody3D
+@export_flags_3d_physics var CollisionMask : int
+
 @export var ItemSlotsCount : int = 4
 @export var InventoryGrid : GridContainer
 @export var InventorySlotPrefab : PackedScene = preload("res://Charlie/Inventory/Inv_Slot.tscn")
@@ -15,9 +18,44 @@ func _ready():
 		var slot = InventorySlotPrefab.instantiate() as InventorySlot
 		InventoryGrid.add_child(slot)
 		slot.InventorySlotID = i
+		slot.OnItemDropped.connect(ItemDroppedOnSlot.bind())
 		InventorySlots.append(slot)
-		
-		
+
+func ItemDroppedOnSlot(fromSlotID : int, toSlotID : int):
+	var toSlotItem = InventorySlots[toSlotID].SlotData
+	var fromSlotItem = InventorySlots[fromSlotID].SlotData
+	
+	InventorySlots[toSlotID].FillSlot(fromSlotItem)
+	InventorySlots[fromSlotID].FillSlot(toSlotItem)
+
+func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
+	return typeof(data) == TYPE_DICTIONARY and data["TYPE"] == "Item"
+	
+func _drop_data(at_position: Vector2, data: Variant) -> void:
+	var newItem = InventorySlots[data["ID"]].SlotData.ItemModelPrefab.instantiate() as Node3D
+	InventorySlots[data["ID"]].FillSlot(null)
+	
+	PlayerBody.get_parent().add_child(newItem)
+	newItem.global_position = GetWorldMousePosition()
+	
+func GetWorldMousePosition() -> Vector3:
+	var mousePos = get_viewport().get_mouse_position()
+	var cam = get_viewport().get_camera_3d()
+	var ray_start = cam.project_ray_origin(mousePos)
+	var ray_end = ray_start + cam.project_ray_normal(mousePos) * cam.global_position.distance_to(PlayerBody.global)
+	var world3d : World3D = PlayerBody.get_world_3d()
+	var space_state = world3d.direct_space_state
+	
+	var query = PhysicsRayQueryParameters3D.create(ray_start, ray_end, CollisionMask)
+	
+	var results = space_state.intersect_ray(query)
+	if (results):
+		return results["position"] as Vector3 + Vector3(0.0, 0.5, 0.0)
+	else:
+		return ray_start.lerp(ray_end, 0.5) + Vector3(0.0, 0.5, 0.0)
+
+
+
 func PickupItem(item : ItemData):
 	for slot in InventorySlots:
 		if (!slot.SlotFilled):
--- Charlie/Inventory/inv_slot.gd
+++ Charlie/Inventory/inv_slot.gd
@@ -2,12 +2,39 @@ extends Panel
 
 class_name InventorySlot
 
+signal OnItemDropped(fromSlotID, toSlotID)
 @export var IconSlot : TextureRect
+
 var InventorySlotID : int = -1
 var SlotFilled : bool = false
 var SlotData : ItemData
 
+
+
 func FillSlot(data: ItemData):
 	SlotData = data
-	SlotFilled = true
-	IconSlot.texture = data.Icon
+	if (SlotData != null):
+		SlotFilled = true
+		IconSlot.texture = data.Icon
+	else:
+		SlotFilled = false
+		IconSlot.texture = null
+		
+func _get_drag_data(at_position: Vector2) -> Variant:
+	if (SlotFilled):
+		var preview : TextureRect = TextureRect.new()
+		preview.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
+		preview.size = IconSlot.size
+		preview.pivot_offset = IconSlot.size / 2.0
+		preview.texture = IconSlot.texture
+		set_drag_preview(preview)
+		
+		return {"Type": "Item", "ID": InventorySlotID}
+	else:
+		return false
+
+func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
+	return typeof(data) == TYPE_DICTIONARY and data["Type"] == "Item"
+	
+func _drop_data(at_position: Vector2, data: Variant) -> void:
+	OnItemDropped.emit(data["ID"], InventorySlotID)
