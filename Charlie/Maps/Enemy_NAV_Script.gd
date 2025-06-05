68b12661d60e4b55da7f8fe435187db192535da3
Cartogen <118136480+Cartogen@users.noreply.github.com>
2025-06-05 14:09:34 +1200

    Revert "Fixed Enemy in my map"
    This reverts commit dc12cdd59be0b8615d41252d3abfe85979379950.

--- Charlie/Maps/Enemy_NAV_Script.gd
+++ Charlie/Maps/Enemy_NAV_Script.gd
@@ -1,8 +1,7 @@
 extends Node3D
+
 @onready var target = $Wayne
 
 
-
-
-func _process(delta):
+func _physics_process(_delta):
 	get_tree().call_group("enemy", "target_position", target.global_transform.origin)
