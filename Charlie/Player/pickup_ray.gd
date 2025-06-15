e722dfc88d4d6b4bc30faac9fad12679fcf9de86
Cartogen <118136480+Cartogen@users.noreply.github.com>
2025-06-16 09:44:38 +1200

    Fixed objectives again
    Now all 3 objective work as intended again!

--- Charlie/Player/pickup_ray.gd
+++ Charlie/Player/pickup_ray.gd
@@ -9,6 +9,7 @@ func _process(_delta: float) -> void:
 	if ray.is_colliding():
 		hit = ray.get_collider()
 		Global.current_raycast = hit.name
+		print(hit.name)
 	else:
 		Global.current_raycast = null
 	#print(Global.current_raycast)
@@ -17,6 +18,5 @@ func _process(_delta: float) -> void:
 	#For things like light switches, i need to consider moving this code to a script on the item
 	#instead so the code works for just the instantiated object!
 	if Input.is_action_pressed("F") and Global.current_raycast == "Sattelite" and Progress.current_objective == 2.5:
-		if Progress.sattelite_box_collected == true:
-			Progress.sattelite_box_collected = false
+		if Global.equipped_item_id == str("Sattelite_Box"):
 			Progress.current_objective = Progress.current_objective + 0.5
