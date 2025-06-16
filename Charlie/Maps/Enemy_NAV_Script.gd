6cb65f9ccd1b40514a80531b0e05e40c9ea38cc6
Cartogen <118136480+Cartogen@users.noreply.github.com>
2025-06-17 10:59:24 +1200

    Preparing the progress tracking

--- Charlie/Maps/Enemy_NAV_Script.gd
+++ Charlie/Maps/Enemy_NAV_Script.gd
@@ -2,7 +2,8 @@ extends Node3D
 @onready var target = $Wayne
 
 
-
+#light level should be 0.05
+#x rotation should be -135.4
 
 func _process(delta):
 	get_tree().call_group("enemy", "target_position", target.global_transform.origin)
--- progression_tracker/progress.gd
+++ progression_tracker/progress.gd
@@ -1,15 +1,40 @@
 extends Node
-var current_objective = 1
-# Objectinve 1 varibles
-var objective_1 = 0
+var current_objective = 0
+# Objective 1 to 1.99 varibles
+var Sattelite_Discovered = false
+var Parts_Discovered = 0
+#When this turns true, spawn enemies, then move to 2.1 so they dont spawn every frame.
+var Sattelite_repaired = false
 
+# Objective 2 to 2.99 varibles
+var weapons_cache_found = false
+var obj_2_enemies_killed = 0
+
+#Objective 3 to 5 varibles
+var impact_site_discovered = false
+var boss_killed = false
+
+
+
+#Objective level notes
+# 0 equals just spawned in. This is where the intro and starting ambiance are built
+#You start with the knife in your inventory/pick it up stright away.
+# apon finding the sattelite, we reach objective 1
+# now when interacting with the sattelite, we get 1.5, find the parts
+# after finding and instering all ___ parts AND fixing the sattelite functions, move to objective 2
+# Spawn in enemies, and start new communications up, now move to objective 2.1.
+# Upon location of the weapons cache, get lots of ammo and the G32 + move to objective 2.5
+# Now killing spree. Once all located enemies are dead, move to objective 3
+# Locate the impact site, move to objective 3.5 as you kill random protecting enemies.
+# Dungeon like gameplay 
+# Finding the boss room sets game to objective 4.
+#Killing the boss ends the game
 
 
 func _process(_delta):
 	#Progressing past objective 1 (killing 5 enemies)
 	if Progress.objective_1 >= 5:
 		current_objective = current_objective + 1
-		objective_1 = 0
 	if Progress.current_objective == 1:
 		pass
 	#If objective 3, win the game
@@ -19,4 +44,5 @@ func _process(_delta):
 	#Player death
 	if Global.health <= 0:
 		get_tree().change_scene_to_file("res://Charlie/UI/credits.tscn")
+		print("You reached objective " + Progress.current_objective)
 		
