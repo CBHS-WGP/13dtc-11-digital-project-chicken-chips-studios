d9b1ae4a94f0300685ef737754e7337af882eac5
Cartogen <118136480+Cartogen@users.noreply.github.com>
2025-06-26 14:58:59 +1200

    Gun now uses bullets

--- Global.gd
+++ Global.gd
@@ -11,6 +11,8 @@ var health = 100
 var settings_open = false
 var current_raycast 
 var equipped_item_id = str("null")
+var G32_bullets = 0
+var pistol_bullets = 12
 func dead(_delta):
 	if health == 0:
 		get_tree().quit()
