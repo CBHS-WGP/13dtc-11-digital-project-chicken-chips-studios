14a4ce16db4e344019b9aa3a9491a97ffdfdbe2b
Cartogen <118136480+Cartogen@users.noreply.github.com>
2025-07-17 14:59:01 +1200

    Starting 2D HUD
    now add actual unique sprites

--- Charlie/UI/hud.gd
+++ Charlie/UI/hud.gd
@@ -4,22 +4,22 @@ var damage_taken
 
 func _process(_delta):
 	if Progress.current_objective == 0 or Progress.current_objective == 0.5:
-		$Main_Objective/Label.text = str("Locate the sattellite for inspection")
+		$Objective_Control/Main_Objective/Label.text = str("Locate the sattellite for inspection")
 	elif Progress.current_objective == 1:
-		$Main_Objective/Label.text = str("Locate the cube for the sattilite!")
+		$Objective_Control/Main_Objective/Label.text = str("Locate the cube for the sattilite!")
 	elif Progress.current_objective == 1.5:
-		$Main_Objective/Label.text = str("Use the parts to repair the sattelite")
+		$Objective_Control/Main_Objective/Label.text = str("Use the parts to repair the sattelite")
 	elif Progress.current_objective == 2.1:
-		$Main_Objective/Label.text = str("Find the weapons cache")
+		$Objective_Control/Main_Objective/Label.text = str("Find the weapons cache")
 	elif Progress.current_objective == 2.6:
-		$Main_Objective/Label.text = str("Kill all 10 enemies!")
-		$Main_Objective/Sub_Objective.visible = true
-		$Main_Objective/Sub_Objective.text = str("Killed:", Progress.obj_2_enemies_killed, "/10")
+		$Objective_Control/Main_Objective/Label.text = str("Kill all 10 enemies!")
+		$Objective_Control/Main_Objective/Sub_Objective.visible = true
+		$Objective_Control/Main_Objective/Sub_Objective.text = str("Killed:", Progress.obj_2_enemies_killed, "/10")
 	elif Progress.current_objective == 3:
-		$Main_Objective/Sub_Objective.visible = false
-		$Main_Objective.text = str("Make the impact site")
+		$Objective_Control/Main_Objective/Sub_Objective.visible = false
+		$Objective_Control/Main_Objective.text = str("Make the impact site")
 	else:
-		$Main_Objective/Label.text = str("TBC")
+		$Objective_Control/Main_Objective/Label.text = str("TBC")
 
 	if Global.health < 100:
 		Global.health = Global.health + 0.01
