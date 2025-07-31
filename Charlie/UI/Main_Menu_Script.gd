bcd7578fe45ddf69a12fc6f938efa1e102bb7819
Cartogen <EverittCD21@student.cbhs.school.nz>
2025-07-31 19:38:19 +1200

    Added pregame cutscene + title theme updated + extra UI

--- Charlie/UI/Main_Menu_Script.gd
+++ Charlie/UI/Main_Menu_Script.gd
@@ -1,6 +1,8 @@
 extends Control
 
+var a = false
 func _ready():
+	$Play_Nodes/Label.visible = false
 	#Sets the resolution to the default when starting up the game.
 	get_window().size = Global.resolution
 	size = Global.resolution
@@ -13,10 +15,16 @@ func _on_exit_pressed():
 	
 #begins the game
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://Charlie/Maps/Test_Map.tscn")
+	$AnimationPlayer.play("play")
+	a = true
+
 
 func _on_options_pressed():
 	$AnimationPlayer.play("Options_Open")
 
 func _on_back_pressed():
 	$AnimationPlayer.play("Options_Close")
+
+func _process(delta):
+		if a == true and $AnimationPlayer.is_playing() == false:
+			get_tree().change_scene_to_file("res://Charlie/Maps/Test_Map.tscn")
