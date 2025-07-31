286b728633cbe7f94e89a3a8aea6e15547782e78
charles153 <124315288+charles153@users.noreply.github.com>
2025-07-31 14:59:59 +1200

    boss

--- boss.gd
+++ boss.gd
@@ -45,3 +45,15 @@ func _on_inner_detect_area_entered(area: Area3D) -> void:
 
 func _on_damage_checker_area_entered(area: Area3D) -> void:
 	health = health - 20
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	print("hit")
+	Global.shot.emit()
+
+
+func _on_timer_timeout() -> void:
+	if inhitbox == true:
+		print("hit")
+		$AnimationPlayer.play("attac_anim")
+		Global.shot.emit()
