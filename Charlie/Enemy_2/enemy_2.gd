fcbad51a8dc3cf7b4453094de3ec2754b151d4fe
Cartogen <118136480+Cartogen@users.noreply.github.com>
2025-08-27 13:26:17 +1200

    Working on enemy 2

--- Charlie/Enemy_2/enemy_2.gd
+++ Charlie/Enemy_2/enemy_2.gd
@@ -1,17 +1,20 @@
 extends CharacterBody3D
 
 @onready var nav_agent = $NavigationAgent3D
-const SPEED = 2
+const SPEED = 2.5
 const GRAVITY = 9.81
 func _physics_process(delta: float) -> void:
-	if is_on_floor() == false and is_on_wall() == false:
+	if not is_on_floor():
 		velocity.y -= GRAVITY * delta
+	else:
+		velocity.y = 0
 
 	var current_location = global_transform.origin
 	var next_location = nav_agent.get_next_path_position()
 	var new_velocity = (next_location - current_location).normalized() * SPEED
-
-	velocity = velocity.move_toward(new_velocity, .25)
+	velocity.x = velocity.move_toward(new_velocity, 0.25)[0]
+	#velocity.y = velocity.move_toward(new_velocity, 0.25)[1]
+	velocity.z = velocity.move_toward(new_velocity, 0.25)[2]
 	move_and_slide()
 
 func target_location(target_location):
