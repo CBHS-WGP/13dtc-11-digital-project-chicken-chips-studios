e2639ca1c77e0e6dd3fd6fc5569f0ebb87a27e8d
Cartogen <118136480+Cartogen@users.noreply.github.com>
2025-09-04 19:05:46 +1200

    Am i the goat, or the goat?
    Enemy gravity implemented
    There are obvious jitter issues which I will try to remedy later, however he now can go up and down slopes, aswell as jump at the player (the rotation and animation isnt quite right yet....)

--- Charlie/Enemy_2/enemy_2.gd
+++ Charlie/Enemy_2/enemy_2.gd
@@ -12,6 +12,8 @@ var new_velocity
 var pos
 var health = 20
 var on_floor = false
+var finding_floor = true
+var can_jump = true
 const SPEED = 4
 const GRAVITY = 9.81
 func _ready() -> void:
@@ -19,8 +21,6 @@ func _ready() -> void:
 	$Model/Freakboy/AnimationPlayer.play("Ball")
 
 func _physics_process(delta: float) -> void:
-	print(animate)
-	$Model/Freakboy.rotation.x = $Model/Freakboy.rotation.x + 0.08
 	jump_ray()
 	floor_ray()
 	if animate == true:
@@ -35,17 +35,15 @@ func _physics_process(delta: float) -> void:
 			launcher.enabled = true
 			animate = false
 	else:
+		$Model/Freakboy.rotation.x = $Model/Freakboy.rotation.x + 0.08
 		current_location = global_transform.origin
 		next_location = nav_agent.get_next_path_position()
 		new_velocity = (next_location - current_location).normalized() * SPEED
 		velocity.x = velocity.move_toward(new_velocity, 0.25)[0]
 		velocity.z = velocity.move_toward(new_velocity, 0.25)[2]
-		#ajust upwards y
-		#ajust downwards y (buggy)
-		if floor.is_colliding() == false:
+		if on_floor == false:
 			velocity.y -= GRAVITY * delta
-		else:
-			velocity.y = 0
+
 	
 	#having on ly the actual model mode to look at the player
 	$Model.look_at(player.global_position)
@@ -56,21 +54,39 @@ func target_location(target_location):
 	nav_agent.set_target_position(target_location)
 	
 func jump_ray():
-	if launcher.is_colliding() and launcher.enabled == true:
-		print("scan")
+	if launcher.is_colliding() and launcher.enabled == true and can_jump == true:
 		hit = launcher.get_collider()
 		pos = player.global_position
-		velocity.y = SPEED * 1.5
-		velocity.x = velocity[0] * 1.5
-		velocity.z = velocity[2] * 1.5
 		launcher.enabled = false
 		animate = true
+		
+		#starting the timers to ensure the jump and floor collisions are turned off for a bit.
+		finding_floor = false
+		$Model/Freakboy/AnimationPlayer.play("Attack_001")
+		$Model/Freakboy.rotation = lerp(Vector3($Model/Freakboy.rotation), Vector3(0, 24, 0), 0.1)
+		$Launcher/Launch_Delay.start()
+		$"Launcher/Stop overlap".start()
+		velocity.y = SPEED * 1.5
+		velocity.x = velocity[0] * 1.03
+		velocity.z = velocity[2] * 1.03
+		can_jump = false
 
 func floor_ray():
 	print(floor.is_colliding())
-	if floor.is_colliding():
+	if floor.is_colliding() == true and finding_floor == true:
 		on_floor = true
 		velocity.y = 0
 		pos = floor.get_collision_point()
+		
 		if pos[1] < floor.position[1]:
-			global_position[1] = global_position[1] + (pos[1] -  0.2)
+			global_position[1] = move_toward(global_position[1], global_position[1] + (pos[1] + 1.2), 0.25)
+	else:
+		on_floor = false
+
+func _on_stop_overlap_timeout() -> void:
+	finding_floor = true
+	
+
+
+func _on_launch_delay_timeout() -> void:
+	can_jump = true
