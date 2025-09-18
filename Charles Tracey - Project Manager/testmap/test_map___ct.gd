e6436de19e171303e84f19b040a084fb193d7bb9
charles153 <traceycw21@student.cbhs.school.nz>
2025-09-18 22:42:47 +1200

    Comments

--- Charles Tracey - Project Manager/testmap/test_map___ct.gd
+++ Charles Tracey - Project Manager/testmap/test_map___ct.gd
@@ -1,5 +1,6 @@
 extends Node3D
 @onready var target = $"Wayne/Enemy detect"
 
+#Updates the target position relative to the enemy.
 func _process(delta):
 	get_tree().call_group("enemy", "target_position", target.global_transform.origin)
